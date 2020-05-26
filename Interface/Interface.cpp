b2a5cbe774bc8b06480c63d9a61675ed77e5a067
Pascal Rene Baehr <pascal.baehr@cern.ch>
2020-05-26 12:12:04 +0200

    [DEV][GPU] Fixed CMake files for GPU build

--- Interface/Interface.cpp
+++ Interface/Interface.cpp
@@ -92,10 +92,6 @@ Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 
 #include "../../src/versionId.h"
 
-#ifdef MOLFLOW
-#include "../src/GPUSim/MolflowModelParser.h"
-#endif
-
 extern Worker worker;
 extern std::vector<std::string> formulaPrefixes;
 //extern const char* appTitle;
