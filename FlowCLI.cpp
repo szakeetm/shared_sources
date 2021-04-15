af6ac9bf94ab81dab39ba79329b106d6c1f29cb1
pbahr <pascal.baehr@cern.ch>
2021-04-15 18:08:58 +0200

    [MPI] Made MPI an optional CMake option, based on availability

--- FlowCLI.cpp
+++ FlowCLI.cpp
@@ -16,11 +16,18 @@
 #include <omp.h>
 #include <sstream>
 #include <Helper/Chronometer.h>
+
+#if defined(USE_MPI)
 #include <mpi.h>
 #include <cereal/archives/binary.hpp>
 #include <cereal/archives/xml.hpp>
 #include <bitset>
+#endif
 
+namespace MFMPI {
+    int world_rank{0};
+    int world_size{1};
+};
 
 static constexpr const char* molflowCliLogo = R"(
   __  __     _  __ _             ___ _    ___
@@ -29,6 +36,7 @@ static constexpr const char* molflowCliLogo = R"(
  |_|  |_\___/_|_| |_\___/\_/\_/ \___|____|___|
     )";
 
+#if defined(USE_MPI)
 template<class T>
 int MPI_Send_serialized(const T &data, int dest, int tag, MPI_Comm comm){
     std::ostringstream state_stream;
@@ -38,13 +46,13 @@ int MPI_Send_serialized(const T &data, int dest, int tag, MPI_Comm comm){
     }
     const auto serialized = state_stream.str();
     //printf("[%d] Attempt to send state to %d (in %lu / %lu bytes).\n", dest, 0, state_stream.str().size(), strlen(state_stream.str().c_str()));
-    return MPI_Send(serialized.data(), (int)serialized.size(), MPI::BYTE, dest, tag, MPI_COMM_WORLD);
+    return MPI_Send(serialized.data(), (int)serialized.size(), MPI::BYTE, dest, tag, comm);
 }
 
 template<class T>
 int MPI_Recv_serialized(T &data, int source, int tag, MPI_Comm comm, MPI_Status *status){
     //Get number of bytes in incoming message
-    MPI_Probe(source, tag, MPI_COMM_WORLD, status);
+    MPI_Probe(source, tag, comm, status);
     int number_bytes;
     MPI_Get_count(status, MPI::BYTE, &number_bytes);
     //printf("Trying to receive %d bytes from %d.\n", number_bytes, source);
@@ -52,7 +60,7 @@ int MPI_Recv_serialized(T &data, int source, int tag, MPI_Comm comm, MPI_Status 
     std::vector<char> incoming(number_bytes);
 
     //Receive the data
-    auto ret = MPI_Recv(incoming.data(), number_bytes, MPI::BYTE, source, tag, MPI_COMM_WORLD, status);
+    auto ret = MPI_Recv(incoming.data(), number_bytes, MPI::BYTE, source, tag, comm, status);
     std::stringstream state_stream;
     state_stream.write(incoming.data(), number_bytes);
 
@@ -64,6 +72,7 @@ int MPI_Recv_serialized(T &data, int source, int tag, MPI_Comm comm, MPI_Status 
 
     return ret;
 }
+#endif // USE_MPI
 
 struct MolflowData{
     // Geometry
@@ -89,28 +98,28 @@ int main(int argc, char** argv) {
     std::setlocale(LC_ALL, "C");
 #endif
 
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+#if defined(USE_MPI)
+    {
+        // Initialize the MPI environment
+        MPI_Init(nullptr, nullptr);
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+        // Get the number of processes
+        MPI_Comm_size(MPI_COMM_WORLD, &MFMPI::world_size);
+        // Get the rank of the process
+        MPI_Comm_rank(MPI_COMM_WORLD, &MFMPI::world_rank);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+        // Get the name of the processor
+        char processor_name[MPI_MAX_PROCESSOR_NAME];
+        int name_len;
+        MPI_Get_processor_name(processor_name, &name_len);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
 
-    if(world_rank == 0)
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+#endif //USE_MPI
+
+    if(MFMPI::world_rank == 0)
         std::cout << molflowCliLogo << std::endl;
-    MPI_Barrier(MPI_COMM_WORLD);
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
 
     SimulationManager simManager{};
     simManager.interactiveMode = true;
@@ -122,14 +131,16 @@ int main(int argc, char** argv) {
         exit(41);
     }
 
+#if defined(USE_MPI)
     {
         // Check if all worlds have the inputfile
-        for (int i = 1; i < world_size; i++) {
+        // forward if not, maybe change to broadcast
+        for (int i = 1; i < MFMPI::world_size; i++) {
             MPI_Barrier(MPI_COMM_WORLD);
-            if (world_rank == 0) {
-                printf("File transfer loop %d / %d.\n", i, world_size);
+            if (MFMPI::world_rank == 0) {
+                printf("File transfer loop %d / %d.\n", i, MFMPI::world_size);
             }
-            if (world_rank == 0) {
+            if (MFMPI::world_rank == 0) {
                 bool hasFile[1] {false};
                 MPI_Status status;
                 // Receive at most MAX_NUMBERS from process zero
@@ -145,15 +156,15 @@ int main(int argc, char** argv) {
                     contents.assign(std::istreambuf_iterator<char>(infile),
                                     std::istreambuf_iterator<char>());
                     printf("Attempt to write file to %d.\n", i);
-                    MPI_Send(contents.c_str(), contents.size()*MPI::BYTE.Get_size(), MPI::BYTE, i, 0, MPI_COMM_WORLD);
+                    MPI_Send(contents.c_str(), MPI::BYTE.Get_size() * contents.size(), MPI::BYTE, i, 0, MPI_COMM_WORLD);
                 }
 
-            } else if(world_rank == i){
+            } else if(MFMPI::world_rank == i){
                 bool hasFile[] {false/*!Settings::inputFile.empty()*/};
                 MPI_Send(hasFile, 1, MPI::BOOL, 0, 0, MPI_COMM_WORLD);
 
                 if(!hasFile[0]){
-                    printf("[%d] Attempt to receive file from 0.\n", world_rank);
+                    printf("[%d] Attempt to receive file from 0.\n", MFMPI::world_rank);
                     MPI_Status status;
                     // Probe for an incoming message from process zero
                     MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
@@ -179,6 +190,7 @@ int main(int argc, char** argv) {
             }
         }
     }
+#endif
 
     if(Initializer::initFromFile(argc, argv, &simManager, &model, &globState)){
         exit(42);
@@ -249,13 +261,13 @@ int main(int argc, char** argv) {
         }
         else if(!Settings::autoSaveDuration && (uint64_t)(elapsedTime)%60==0){
             if(Settings::simDuration > 0){
-                printf("[%.0lfs / %lf] %llu Hit : %e Hit/s\n", Settings::simDuration-elapsedTime, elapsedTime, globState.globalHits.globalHits.nbMCHit - oldHitsNb, (double)(globState.globalHits.globalHits.nbMCHit-oldHitsNb)/(elapsedTime));
+                printf("[%.0lfs / %lf] %llu Hit : %e Hit/s\n", (double) Settings::simDuration - elapsedTime, elapsedTime, globState.globalHits.globalHits.nbMCHit - oldHitsNb, (double)(globState.globalHits.globalHits.nbMCHit-oldHitsNb)/(elapsedTime));
             }
         }
 
         // Check for potential time end
         if(Settings::simDuration > 0) {
-            endCondition |= elapsedTime >= Settings::simDuration;
+            endCondition |= (elapsedTime >= (double) Settings::simDuration);
         }
     } while(!endCondition);
     simTimer.Stop();
@@ -275,6 +287,7 @@ int main(int argc, char** argv) {
                               (elapsedTime) << "Des/s\n";
     }
 
+#if defined(USE_MPI)
     {
         // First prepare receive structure
         MPI_Status status;
@@ -284,25 +297,25 @@ int main(int argc, char** argv) {
         // Retrieve global simu state from other ranks
         // Binary Fan-in pattern
         MPI_Barrier(MPI_COMM_WORLD);
-        for (int k = 0; k < std::ceil(std::log2(world_size)); k++) {
-            if(world_rank==0)
+        for (int k = 0; k < std::ceil(std::log2(MFMPI::world_size)); k++) {
+            if(MFMPI::world_rank==0)
                 printf("\n");
             MPI_Barrier(MPI_COMM_WORLD);
-            for (int i = 0; i < world_size; i++) {
+            for (int i = 0; i < MFMPI::world_size; i++) {
                 MPI_Barrier(MPI_COMM_WORLD);
-                if (world_rank == i || world_rank == i + std::pow(2,k)) {
+                if (MFMPI::world_rank == i || MFMPI::world_rank == i + std::pow(2,k)) {
                     std::bitset<32> procTurn(i);
                     if(procTurn.test(k)) { // is kth bit set in i ?
                         continue;
                     }
-                    else if(i + std::pow(2,k) < world_size) {
+                    else if(i + std::pow(2,k) < MFMPI::world_size) {
                         // _i <- _i + _(i+2^k)
-                        if (world_rank == i) {
+                        if (MFMPI::world_rank == i) {
                             //printf("..Receive simu state loop %d from %d (.. %d).\n", i, i + (int) std::pow(2, k),(int) std::ceil(std::log2(world_size)));
                             MPI_Recv_serialized(tmpState, (int)(i + std::pow(2,k)), 0, MPI_COMM_WORLD, &status);
                             globState += tmpState;
                         }
-                        else if (world_rank == i + std::pow(2,k)) {
+                        else if (MFMPI::world_rank == i + std::pow(2,k)) {
                             //printf("..Send simu state loop %d to %d (.. %d).\n", i + (int)std::pow(2,k), i,(int)std::ceil(std::log2(world_size)));
                             MPI_Send_serialized(globState, i, 0, MPI_COMM_WORLD);
                         }
@@ -314,9 +327,10 @@ int main(int argc, char** argv) {
 
     // Finalize the MPI environment.
     MPI_Finalize();
-    if(world_rank != 0){
+    if(MFMPI::world_rank != 0){
         return 0;
     }
+#endif //USE_MPI
 
     printf("Total after sum up hits: %zu\n", globState.globalHits.globalHits.nbMCHit);
     if(elapsedTime > 1e-4) {
@@ -357,24 +371,19 @@ int main(int argc, char** argv) {
         }
     }*/
 
-    /*if(world_rank == 0){
+    if(MFMPI::world_rank == 0){
         // Export results
         char outname[256]{};
-        sprintf(outname, "out%d_%s", world_rank, std::filesystem::path(Settings::outputFile).filename().c_str());
+        sprintf(outname, "out%d_%s", MFMPI::world_rank, std::filesystem::path(Settings::outputFile).filename().c_str());
         if(Settings::inputFile != outname){
             // Copy full file description first, in case outputFile is different
-            printf("[%d] attempting to copy file %s to %s\n", world_rank, std::filesystem::relative(Settings::inputFile).c_str(), outname);
+            printf("[%d] attempting to copy file %s to %s\n", MFMPI::world_rank, std::filesystem::relative(Settings::inputFile).c_str(), outname);
             std::filesystem::copy_file( std::filesystem::relative(Settings::inputFile), outname,
                                         std::filesystem::copy_options::overwrite_existing);
         }
         FlowIO::WriterXML writer;
         writer.SaveSimulationState(outname, &model, globState);
-    }*/
-
-    // Finalize the MPI environment.
-    //MPI_Finalize();
-    return 0;
-    exit(42);
+    }
 
     return 0;
 }
