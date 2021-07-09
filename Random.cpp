c1d3eae4b90ee097e73c43ccc19471b8be48e5fb
Pascal Rene Baehr <pascal.baehr@cern.ch>
2021-07-09 17:04:17 +0200

    [CMake] Removed trace omp includes

--- Random.cpp
+++ Random.cpp
@@ -28,8 +28,6 @@ Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 #include <process.h>
 #else
 #include <unistd.h>
-#include <omp.h>
-
 #endif // def WIN
 
 #define  RK_STATE_LEN 624
