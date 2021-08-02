2e016894cbd15db900b1655f96da9082c21f732f
Pascal Rene Baehr <pascal.baehr@cern.ch>
2021-08-02 09:37:28 +0200

    [CMake] Fixed incorrect check for build type match

--- simulator.cmake
+++ simulator.cmake
@@ -85,7 +85,7 @@ target_compile_options(${PROJECT_NAME} PRIVATE
         $<$<CXX_COMPILER_ID:MSVC>:
         /W4>)
 # Preprocessor definitions
-if(CMAKE_BUILD_TYPE MATCHES (Debug|RelWithDebInfo))
+if(CMAKE_BUILD_TYPE MATCHES Debug|RelWithDebInfo)
     target_compile_definitions(${PROJECT_NAME} PRIVATE)
     if(MSVC)
         target_compile_options(${PROJECT_NAME} PRIVATE /MDd /Od /EHsc)
