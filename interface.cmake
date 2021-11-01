80d4296421cfd29437a7bc82fa4e742790769d69
Pascal Rene Baehr <pascal.baehr@cern.ch>
2021-11-01 15:03:56 +0100

    [Bugfix][CMake] Fixed commented std 17 compile definition, resulting in failing Windows builds

--- interface.cmake
+++ interface.cmake
@@ -155,9 +155,9 @@ endif(NOT MSVC)
 # Defines Flags for Windows and Linux                      #
 ############################################################
 
-#[[
+
 target_compile_features(${PROJECT_NAME} PRIVATE cxx_std_17)
-target_compile_options(${PROJECT_NAME} PRIVATE
+#[[target_compile_options(${PROJECT_NAME} PRIVATE
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
         -Wall>
         $<$<CXX_COMPILER_ID:MSVC>:
