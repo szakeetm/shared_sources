0441181103d3e8b0a6655c9b03356e48ff3687f5
Pascal Rene Baehr <pascal.baehr@cern.ch>
2021-11-19 15:25:25 +0100

    [Fix] Fixed debug error on Windows for debug printting

--- Helper/OutputHelper.h
+++ Helper/OutputHelper.h
@@ -8,6 +8,16 @@
 #ifdef DEBUG
 #define DEBUG 1
 #endif
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#if defined(DEBUG) && DEBUG > 3
+#define DEBUG_PRINT(fmt, ...) printf("DEBUG: %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#elif defined(DEBUG) && DEBUG > 0
+#define DEBUG_PRINT(fmt, ...) printf("DEBUG: %s(): " fmt, __func__, ##__VA_ARGS__)
+#else
+#define DEBUG_PRINT(fmt, ...) /* Don't do anything in release builds */
+#endif
+#else // not Windows
 #if defined(DEBUG) && DEBUG > 3
 #define DEBUG_INT(fmt, ...) printf("DEBUG: %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__)
 #define DEBUG_PRINT(...) DEBUG_INT(__VA_ARGS__)
@@ -17,6 +27,7 @@
 #else
 #define DEBUG_PRINT(fmt, ...) /* Don't do anything in release builds */
 #endif
+#endif // Windows
 
 namespace Util {
 
