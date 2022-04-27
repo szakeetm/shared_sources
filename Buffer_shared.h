6a1a36aa39a52e35bdd797943ac6d8740f7c7953
Marton Ady <marton.ady@cern.ch>
2022-04-27 15:18:50 +0200

    add impulse quantitites

--- Buffer_shared.h
+++ Buffer_shared.h
@@ -523,28 +523,34 @@ public:
 #if defined(MOLFLOW)
 struct FacetHitBuffer {
 
-    FacetHitBuffer() {
-        nbDesorbed = 0;
-        nbMCHit = 0;
-        nbHitEquiv = 0.0;
-        nbAbsEquiv = 0.0;
-        sum_1_per_ort_velocity = 0.0;
-        sum_1_per_velocity = 0.0;
-        sum_v_ort = 0.0;
-    }
-    FacetHitBuffer& operator+=(const FacetHitBuffer& rhs);
+	FacetHitBuffer() {
+		nbDesorbed = 0;
+		nbMCHit = 0;
+		nbHitEquiv = 0.0;
+		nbAbsEquiv = 0.0;
+		sum_1_per_ort_velocity = 0.0;
+		sum_1_per_velocity = 0.0;
+		sum_v_ort = 0.0;
+		impulse = Vector3d(0.0, 0.0, 0.0);
+		impulse_square = Vector3d(0.0, 0.0, 0.0);
+		impulse_momentum = Vector3d(0.0, 0.0, 0.0);
+	}
+	FacetHitBuffer& operator+=(const FacetHitBuffer& rhs);
 
-    // Counts
-    size_t nbDesorbed;          // Number of desorbed molec
-    size_t nbMCHit;               // Number of hits
-    double nbHitEquiv;			//Equivalent number of hits, used for low-flux impingement rate and density calculation
-    double nbAbsEquiv;          // Equivalent number of absorbed molecules
-    double sum_1_per_ort_velocity;    // sum of reciprocials of orthogonal velocity components, used to determine the density, regardless of facet orientation
-    double sum_1_per_velocity;          //For average molecule speed calculation
-    double sum_v_ort;          // sum of orthogonal speeds of incident velocities, used to determine the pressure
+	// Counts
+	size_t nbDesorbed;          // Number of desorbed molec
+	size_t nbMCHit;               // Number of hits
+	double nbHitEquiv;			//Equivalent number of hits, used for low-flux impingement rate and density calculation
+	double nbAbsEquiv;          // Equivalent number of absorbed molecules
+	double sum_1_per_ort_velocity;    // sum of reciprocials of orthogonal velocity components, used to determine the density, regardless of facet orientation
+	double sum_1_per_velocity;          //For average molecule speed calculation
+	double sum_v_ort;          // sum of orthogonal speeds of incident velocities, used to determine the pressure
+	Vector3d impulse; //sum of impulse changes exerted to the facet by inbound and outbound molecules, for force calculation
+	Vector3d impulse_square; //sum of impulse change squares exerted to the facet by inbound and outbound molecules, for noise estimation
+	Vector3d impulse_momentum; //sum of impulse momentum changes exerted to the facet by inbound and outbound molecules relative to user-defined axis, for torque calculation
 
 	template<class Archive>
-	void serialize(Archive & archive)
+	void serialize(Archive& archive)
 	{
 		archive(
 			CEREAL_NVP(nbDesorbed),
