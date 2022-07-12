2947b33ac5a55a435500c3a8a750760dfc2ffebf
Marton Ady <marton.ady@cern.ch>
2022-07-12 16:31:20 +0200

    Fix graham scan, scale-invariant collinear condition

--- Geometry_shared.cpp
+++ Geometry_shared.cpp
@@ -445,8 +445,8 @@ void Geometry::CreatePolyFromVertices_Convex() {
 	loopLength = ii;
 	//End graham scan
 
-	std::vector<size_t> vertexIds(selectedVertices.size());
-	for (size_t i = 0; i < selectedVertices.size(); i++) {
+	std::vector<size_t> vertexIds(loopLength);
+	for (size_t i = 0; i < loopLength; i++) {
 		vertexIds[i] = selectedVertices[returnList[i]];
 	}
 
@@ -3236,8 +3236,6 @@ void Geometry::MergecollinearSides(InterfaceFacet *f, double lT) {
 void Geometry::CalculateFacetParams(InterfaceFacet* f) {
 	// Calculate facet normal
 	Vector3d p0 = vertices3[f->indices[0]];
-	Vector3d v1;
-	Vector3d v2;
 	bool consecutive = true;
 	int ind = 2;
 
@@ -3247,10 +3245,10 @@ void Geometry::CalculateFacetParams(InterfaceFacet* f) {
 	while (ind < f->sh.nbIndex && consecutive) {
 		size_t i2 = f->indices[ind++];
 
-		v1 = vertices3[i1] - vertices3[i0]; // v1 = P0P1
-		v2 = vertices3[i2] - vertices3[i1]; // v2 = P1P2
-		f->sh.N = CrossProduct(v1, v2);              // Cross product
-		consecutive = (f->sh.N.Norme() < 1e-3);
+		auto v1 = vertices3[i1] - vertices3[i0]; // v1 = P0P1
+		auto v2 = vertices3[i2] - vertices3[i1]; // v2 = P1P2
+		f->sh.N = CrossProduct(v1.Normalized(), v2.Normalized()); // Scale-invariant cross product
+		consecutive = (f->sh.N.Norme() < 1e-10);
 	}
 	f->collinear = consecutive; //mark for later that this facet was on a line
 	f->sh.N = f->sh.N.Normalized();                  // Normalize
