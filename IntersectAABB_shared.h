2a57b531e7c00d8a3d6bd8528801553a76d73ee5
Marton Ady <marton.ady@cern.ch>
2023-05-04 12:45:31 +0200

    more const & to const

--- IntersectAABB_shared.h
+++ IntersectAABB_shared.h
@@ -44,13 +44,13 @@ public:
 AABBNODE *BuildAABBTree(const std::vector<SimulationFacet *> &facets, const size_t depth, size_t& maxDepth);
 
 void IntersectTree(MFSim::ParticleTracer &currentParticleTracer, const AABBNODE &node, const Vector3d &rayPos,
-                   const Vector3d &rayDirOpposite, SimulationFacet *const lastHitBefore, const bool &nullRx,
-                   const bool &nullRy, const bool &nullRz, const Vector3d &inverseRayDir, bool &found,
+                   const Vector3d &rayDirOpposite, SimulationFacet *const lastHitBefore, const bool nullRx,
+                   const bool nullRy, const bool nullRz, const Vector3d &inverseRayDir, bool &found,
                    SimulationFacet *&collidedFacet, double &minLength);
 std::tuple<bool, SimulationFacet *, double>
 Intersect(MFSim::ParticleTracer &currentParticleTracer, const Vector3d &rayPos, const Vector3d &rayDir, const AABBNODE *bvh);
 /*bool Visible(Simulation *sHandle, Vector3d *c1, Vector3d *c2, SubprocessFacet *f1, SubprocessFacet *f2,
              CurrentParticleStatus &currentParticleTracer);*/
-bool IsInFacet(const SimulationFacet &f, const double &u, const double &v);
+bool IsInFacet(const SimulationFacet &f, const double u, const double v);
 //Vector3d PolarToCartesian(const SubprocessFacet *const collidedFacet, const double theta, const double phi, const bool reverse); //sets sHandle->currentParticleTracer.direction
 //std::tuple<double, double> CartesianToPolar(const Vector3d& incidentDir, const Vector3d& normU, const Vector3d& normV, const Vector3d& normN);
