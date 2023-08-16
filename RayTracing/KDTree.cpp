20cbddaf7be06ba2aff9d0e8387f5faa1ec27a54
Marton Ady <marton.ady@cern.ch>
2023-08-16 16:49:58 +0200

    include memcpy <cstring>

--- RayTracing/KDTree.cpp
+++ RayTracing/KDTree.cpp
@@ -28,6 +28,7 @@ Full license text: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 #include <cmath>
 #include <set>
 #include <cassert>
+#include <cstring>
 
 #include "KDTree.h"
 #include "Ray.h"
