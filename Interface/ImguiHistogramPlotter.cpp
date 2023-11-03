e918c90b0323718e6629fbd9e7627b95b7a51add
Tymoteusz Mroczkowski <tymoteusz.mroczkowski@cern.ch>
2023-11-03 15:48:44 +0100

    feat: Imgui histogram layout

--- Interface/ImguiHistogramPlotter.cpp
+++ Interface/ImguiHistogramPlotter.cpp
@@ -1,12 +1,53 @@
 #include "ImguiHistogramPlotter.h"
 #include "imgui.h"
+#include "implot/implot.h"
+#include "imgui_stdlib/imgui_stdlib.h"
 
 void ImHistogramPlotter::Draw()
 {
 	if (!drawn) return;
 	float dummyWidth;
 	ImGui::SetNextWindowPos(ImVec2(3 * txtW, 4 * txtW), ImGuiCond_FirstUseEver);
-	ImGui::SetNextWindowSizeConstraints(ImVec2(txtW * 70, txtH * 15), ImVec2(1000 * txtW, 100 * txtH));
+	ImGui::SetNextWindowSizeConstraints(ImVec2(txtW * 85, txtH * 15), ImVec2(1000 * txtW, 100 * txtH));
 	ImGui::Begin("Histogram Plotter", &drawn, ImGuiWindowFlags_NoSavedSettings);
+
+	if (ImGui::BeginTabBar("Histogram types")) {
+		
+		if (ImGui::BeginTabItem("Bounces before absorption")) {
+			plotTab = bounces;
+			ImGui::EndTabItem();
+		}
+		if (ImGui::BeginTabItem("Flight distance before absorption")) {
+			plotTab = distance;
+			ImGui::EndTabItem();
+		}
+		if (ImGui::BeginTabItem("Flight time before absorption")) {
+			plotTab = time;
+			ImGui::EndTabItem();
+		}
+		ImGui::EndTabBar();
+	}
+	DrawPlot();
+	if(ImGui::Button("<< Hist settings")) {} ImGui::SameLine();
+	
+	ImGui::SetNextItemWidth(txtW * 20);
+	if(ImGui::BeginCombo("##HIST","")) {
+		ImGui::EndCombo();
+	} ImGui::SameLine();
+	if (ImGui::Button("<- Show Facet")) {} ImGui::SameLine();
+	if (ImGui::Button("Add")) {} ImGui::SameLine();
+	if (ImGui::Button("Remove")) {} ImGui::SameLine();
+	if (ImGui::Button("Remove all")) {} ImGui::SameLine();
+	ImGui::Checkbox("Normalize", &normalize);
 	ImGui::End();
+}
+
+void ImHistogramPlotter::DrawPlot()
+{
+	if(plotTab==bounces) xAxisName = "Number of bounces";
+	if(plotTab==distance) xAxisName = "Distance [cm]";
+	if(plotTab==time) xAxisName = "Time [s]";
+	if (ImPlot::BeginPlot("##Histogram", xAxisName.c_str(), 0, ImVec2(ImGui::GetWindowContentRegionWidth(), ImGui::GetWindowSize().y - 4.5 * txtH))) {
+		ImPlot::EndPlot();
+	}
 }
--- Interface/ImguiHistogramPlotter.h
+++ Interface/ImguiHistogramPlotter.h
@@ -4,5 +4,14 @@ class ImHistogramPlotter : public ImWindow {
 public:
 	void Draw();
 protected:
+	//functions
+	void DrawPlot();
 
+	//types
+	enum plotTabs {bounces, distance, time};
+	
+	//variables
+	plotTabs plotTab = bounces;
+	std::string xAxisName = "Number of bounces";
+	bool normalize = false;
 };
