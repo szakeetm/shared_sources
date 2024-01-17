3dd340f7df02e8ef16b5cde7ca5929cfde52a78c
Tymoteusz Mroczkowski <tymoteusz.mroczkowski@cern.ch>
2024-01-17 13:23:55 +0100

    feat: Imgui facet rotate functional

--- Interface/ImguiFacetRotate.h
+++ Interface/ImguiFacetRotate.h
@@ -13,5 +13,6 @@ protected:
 	double a=0, b=0, c=0, u=0, v=0, w=0, deg=0, rad=0;
 	std::string aIn="0", bIn = "0", cIn = "0", uIn = "0", vIn = "0", wIn = "0", degIn = "0", radIn = "0";
 	void RotateFacetButtonPress(bool copy);
-	void DoRotate(bool copy);
+	void GetBaseButtonPress();
+	void CalcDiffButtonPress();
 };
